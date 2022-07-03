910ec1d9d94476a284cd75d411b76c02c85b1a73
Anony <ljx1583861@qq.com>
2022-07-03 21:34:39 +0800

    修改API根路径

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,6 @@
 $.ajaxPrefilter(option => {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = `http://www.liulongbin.top:3007` + option.url;
+    option.url = `http://127.0.0.1:3007` + option.url;
     // 统一为有权限的接口，设置 headers 请求头
     if (option.url.indexOf("/my/") !== -1) {
         option.headers = {
